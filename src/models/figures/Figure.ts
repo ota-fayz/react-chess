f2923d5b0c7acb075b8040840d1e42990a7e02e7
Otabek Fayziev <ota_fayz@MacBook-Pro-Otabek.local>
2022-06-07 10:44:43 +0500

    fc

---
+++ src/models/figures/Figure.ts
@@ -0,0 +1,30 @@
+import { Colors } from "../Colors"
+import logo from "../../assets/black-knight.png"
+import { Cell } from "../Cell"
+
+export enum FigureNames {
+    FIGURE = "",
+    KING = "Король",
+    KNIGHT = "Конь",
+    PAWN = "Пешка",
+    QUEEN = "Ферзь",
+    ROOK = "Ладья",
+    BISHOP = "Слон",
+}
+
+export class Figure {
+    color: Colors
+    logo: typeof logo | null
+    cell: Cell
+    name: FigureNames
+    id: number
+
+    constructor(color: Colors, cell: Cell) {
+        this.color = color
+        this.cell = cell
+        this.cell.figure = this
+        this.logo = null
+        this.name = FigureNames.FIGURE
+        this.id = Math.random()
+    }
+}
